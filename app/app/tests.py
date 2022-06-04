e5ab5deab88aba21de96cb9fca73c7003c06ac35
Tim Wambach <timwambach@Tims-MacBook-Pro.local>
2022-06-04 18:04:06 -0500

    Adding db

--- app/app/tests.py
+++ app/app/tests.py
@@ -5,12 +5,10 @@ from app import calc
 
 class CalcTests(SimpleTestCase):
 
-
     def test_add_numbers(self):
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
-
 
     def test_subtract_numbers(self):
         res = calc.subtract(10, 15)
